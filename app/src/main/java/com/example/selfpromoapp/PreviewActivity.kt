fd5258ffcffbbddc4c496c6e928781cb7362df9d
Leonardo Oliveira <lolive4@sibsnet.local>
2021-11-10 16:43:07 +0000

    Getting the values in PreviewActivity from Message

--- app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
@@ -10,21 +10,21 @@ class PreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
+//        *** BAD WAY TO DO IT ***
+//        val contactName = intent.getStringExtra("Contact Name")
+//        val contactNumber = intent.getStringExtra("Contact Number")
+//        val myDisplayName = intent.getStringExtra("My Display Name")
+//        val includeJunior = intent.getBooleanExtra("Include Junior",false)
+//        val jobTitle = intent.getStringExtra("Job Title")
+//        val immediateStart = intent.getBooleanExtra("Immediate Start",false)
+//        val startDate = intent.getStringExtra("Start Date")
+//
+//        val testString = "Contact Name: $contactName, Contact Number: $contactNumber, My display name: $myDisplayName" +
+//                ", Include Junior: $includeJunior, Job Title: $jobTitle, Immediate Start: $immediateStart, Start Date: $startDate"
+//
+//        text_view_message.text = testString
 
-        val contactName = intent.getStringExtra("Contact Name")
-        val contactNumber = intent.getStringExtra("Contact Number")
-        val myDisplayName = intent.getStringExtra("My Display Name")
-        val includeJunior = intent.getBooleanExtra("Include Junior",false)
-        val jobTitle = intent.getStringExtra("Job Title")
-        val immediateStart = intent.getBooleanExtra("Immediate Start",false)
-        val startDate = intent.getStringExtra("Start Date")
-
-        val testString = "Contact Name: $contactName, Contact Number: $contactNumber, My display name: $myDisplayName" +
-                ", Include Junior: $includeJunior, Job Title: $jobTitle, Immediate Start: $immediateStart, Start Date: $startDate"
-
-        text_view_message.text = testString
-
-
+        val message = intent.getSerializableExtra("Message") as Message
 
     }
 
