6503964c4fb0a2c1a6a564e617b486179a1c9e5d
Leonardo Oliveira <lolive4@sibsnet.local>
2021-11-11 09:37:41 +0000

    lateinit vars

--- app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
@@ -6,6 +6,9 @@ import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
 
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
@@ -24,21 +27,36 @@ class PreviewActivity : AppCompatActivity() {
 //
 //        text_view_message.text = testString
 
-        val message = intent.getSerializableExtra("Message") as Message
-        val messagePreviewText = """
-            Hi ${message.contactName},
-            
-            My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
-            
-            I have a portfolio of apps to demonstrate my technical skills that i can show on request.
-            
-            I am able to start a new position ${message.getAvailability()}.
-            
-            Please get in touch if you have any suitable roles for me.
-            
-            Thanks and best Regards
-        """.trimIndent()
-
+        displayMessage()
+        setupButton()
     }
 
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("Message") as Message
+        messagePreviewText = """
+                Hi ${message.contactName},
+                
+                My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
+                
+                I have a portfolio of apps to demonstrate my technical skills that i can show on request.
+                
+                I am able to start a new position ${message.getAvailability()}.
+                
+                Please get in touch if you have any suitable roles for me.
+                
+                Thanks and best Regards
+            """.trimIndent()
+
+        text_view_message.text = messagePreviewText
+    }
+
+
+    private fun setupButton() {
+
+        button_send_message.setOnClickListener {
+            // Send Message Intent
+        }
+
+
+    }
 }
